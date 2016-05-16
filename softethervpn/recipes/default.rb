4327055cb66681d4adb71aecd2eb600797a90c71
Ahmed Shabrawy <a.shabrawy@cequens.com>
2016-05-16 15:33:16 +0200

    test

--- softethervpn/recipes/default.rb
+++ softethervpn/recipes/default.rb
@@ -7,12 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe      'yum_package'
+
 include_recipe      'build-essential'
 
 # install all the dependencies
+#%w{curl,gcc,binutils,tar,gzip,glibc,zlib,openssl,readline,ncurses,pthread}.each do |pkg|
+#    yum_package pkg do
+#        action  :update
+#    end
+#end
+
+
+
+execute "unpack_ethersoft_vpn" do
 %w{curl,gcc,binutils,tar,gzip,glibc,zlib,openssl,readline,ncurses,pthread}.each do |pkg|
-    yum_package pkg do
-        action  :update
-    end
+    command "yum -y update  #{pkg}"
+    #only_if {File.exists?("/")}
 end
